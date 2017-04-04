ca48c6ecf7dd81bff3fb13268daecd026f048987
afalcozvigne <a.falcozvigne@epsi.fr>
2017-04-04 10:53:03 +0200

    modif index

--- public/index.php
+++ public/index.php
@@ -5,20 +5,10 @@
     <?php include"../include/navBar.php" ?>
     <div class="row">
         <div class="col-md-3">
-
         </div>
         <div class="col-md-6 ">
-            <div class="form-group">
-                <label for="usr">Email :</label>
-                <input type="text" class="form-control" id="usr">
-            </div>
-            <div class="form-group">
-                <label for="pwd">Mot de passe:</label>
-                <input type="password" class="form-control" id="pwd">
-            </div>
         </div>
         <div class="col-md-3">
-
         </div>
 
     </div>
