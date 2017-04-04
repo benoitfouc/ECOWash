b0ae2b82b9a002ff6c7f14cac9e1b89d3bfcbc8d
Benoit Foucher <benoit.foucher@epsi.fr>
2017-04-04 10:23:19 +0200

    Update

---
+++ include/header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexandre
+ * Date: 03/04/2017
+ * Time: 15:07
+ * BDD : u585215212_weco
+ * User : u585215212_weco
+ * Host : mysql.hostinger.fr
+ */
+?>
+<html>
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="../include/jquery-3.2.0.min.js"></script>
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
---
+++ public/maMachine.php
@@ -0,0 +1,26 @@
+<?php include"../include/header.php" ?>
+<title>Ma machine</title>
+</head>
+<body>
+    <?php include"../include/navBar.php" ?>
+    <div class="row">
+            <div class="col-md-3">
+
+        </div>
+        <div class="col-md-6">
+<div class="alert alert-danger">
+  <strong>Attention !</strong> Votre machine présente un problème technique, merci de contacter votre technicien.
+</div>
+        </div>
+
+        <div class="col-md-3">
+
+        </div>
+
+    </div>
+    <script type="text/javascript">
+        document.getElementById("Connexion").className = "";
+        document.getElementById("Machine").className = "active";
+    </script>
+</body>
+</html>
